df6432e04f2e2bad6280bba65dc6ec7bcdd03953
Nestor Monroy <nestor.monroy.90@gmail.com>
2021-10-27 22:47:35 -0500

    [example] send parameters post with request, sesion and contex

--- jsp-servlets/JSPServlets/src/main/java/com/nes/Servlet.java
+++ jsp-servlets/JSPServlets/src/main/java/com/nes/Servlet.java
@@ -3,10 +3,12 @@ package com.nes;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class Servlet
@@ -71,8 +73,33 @@ public class Servlet extends HttpServlet {
 
 		if (accion != null) {
 			if (accion.equals("iniciarSesion")) {
-				System.out.println("Usuario: " + request.getParameter("usuario") + ", Contraseña: "
-						+ request.getParameter("contrasena"));
+//				System.out.println("Usuario: " + request.getParameter("usuario") + ", Contraseña: "
+//						+ request.getParameter("contrasena"));
+
+				String usuario = request.getParameter("usuario");
+				String contrasena = request.getParameter("contrasena");
+
+				// Ámbito Request MAP(Clave-Valor)
+				// Solo existe por UNA sola vez o peticion
+				request.setAttribute("usuario", usuario);
+				request.setAttribute("contrasena", contrasena);
+
+				// Ámbito Sesión
+				// Coloca el objecto mientras la navegacion este abierta ** o la ventana del
+				// navegador este abierta ** Si se cierra la venta, el objecto es destruido
+				HttpSession sesion = request.getSession();
+				sesion.setAttribute("usuario", usuario);
+				sesion.setAttribute("contrasena", contrasena);
+
+				// Ámbito Contexto
+				// Contexto del servlet ** existe mientras el Servelet no ejecute un metodo
+				// destroy()
+				ServletContext contexto = getServletContext();
+				contexto.setAttribute("usuario", usuario);
+				contexto.setAttribute("contrasena", contrasena);
+
+				getServletContext().getRequestDispatcher("/jsp/postLogin.jsp").forward(request, response);
+
 			}
 
 		} else {
--- jsp-servlets/JSPServlets/src/main/webapp/jsp/postLogin.jsp
+++ jsp-servlets/JSPServlets/src/main/webapp/jsp/postLogin.jsp
@@ -10,7 +10,19 @@
 	<h1>postLogin.jsp</h1>
 	<h2>Request</h2>
 	<br />
-	<%="usuario: " + request.getParameter("usuario") + ", contraseña: " + request.getParameter("contrasena")%>
+	<%-- <%="usuario: " + request.getParameter("usuario") + ", contraseña: " + request.getParameter("contrasena")%> --%>
+		<h2>Request</h2> <br/>
+	<%="usuario: " + request.getAttribute("usuario") +
+		", contraseña: " + request.getAttribute("contrasena")%>
+	
+	<h2>Sesion</h2> <br/>
+	<%="usuario: " + session.getAttribute("usuario")
+		+ ", contraseña: " + session.getAttribute("contrasena") %>
+		
+		
+	<h2>Contexto</h2> <br/>
+	<%="usuario: " + application.getAttribute("usuario")
+		+ ", contraseña: " + application.getAttribute("contrasena")%>
 	
 </body>
 </html>
