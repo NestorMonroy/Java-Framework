cc0ed089b97a95d91b8cc187342becfd4cbeb74c
Nestor Monroy <nestor.monroy.90@gmail.com>
2021-11-24 23:42:09 -0600

    [initial] struts2-exceptions-insurance

---
+++ Struts/Apache&Struts2/struts2-exceptions-insurance/src/mx/nestor/insurance/action/Register.java
@@ -0,0 +1,43 @@
+package mx.nestor.insurance.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+import mx.nestor.insurance.model.Agent;
+
+public class Register extends ActionSupport {
+
+	private static final long serialVersionUID = 786961954264911644L;
+
+	private Agent agentBean;
+
+	@Override
+	public String execute() throws Exception {
+
+		// Call back end Service to store agentBean info in database..
+		return SUCCESS;
+	}
+
+	@Override
+	public void validate() {
+		if (agentBean.getFirstName().length() == 0) {
+			addFieldError("agentBean.firstName", "First Name is required");
+		}
+
+		if (agentBean.getLastName().length() == 0) {
+			addFieldError("agentBean.lastName", "Last name is required.");
+		}
+
+		if (agentBean.getEmail().length() == 0) {
+			addFieldError("agentBean.email", "Email is required.");
+		}
+	}
+
+	public Agent getAgentBean() {
+		return agentBean;
+	}
+
+	public void setAgentBean(Agent agentBean) {
+		this.agentBean = agentBean;
+	}
+
+}
